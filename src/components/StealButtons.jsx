49edecd77bbfc8d17434940bd384352d39b48ec0
Jack Matthews <jm5112356@gmail.com>
2023-01-25 17:28:44 -0500

    Add functionality to copy image

--- src/components/StealButtons.jsx
+++ src/components/StealButtons.jsx
@@ -1,8 +1,9 @@
 import { findByProps } from "@vendetta/metro";
 import { Platform } from "@vendetta/metro/common/ReactNative";
-import { setString } from "@vendetta/metro/common/clipboard";
+import { setString, setImage } from "@vendetta/metro/common/clipboard";
 import { getAssetIDByName } from "@vendetta/ui/assets";
 import { showToast } from "@vendetta/ui/toasts";
+import fetchImageAsDataURL from "../functions/fetchImageAsDataURL";
 
 const {
 	default: Button,
@@ -12,7 +13,8 @@ const {
 } = findByProps("ButtonColors", "ButtonLooks", "ButtonSizes");
 const LazyActionSheet = findByProps("openLazy", "hideActionSheet");
 
-const CopyMessageLink = getAssetIDByName("ic_copy_message_link") 
+const CopyMessageLink = getAssetIDByName("ic_copy_message_link");
+const CopyFile = getAssetIDByName('ic_message_copy');
 
 export default function StealButtons({ emojiNode }) {
 	console.log(emojiNode)
@@ -27,13 +29,17 @@ export default function StealButtons({ emojiNode }) {
 				setString(emojiNode.src);
 				LazyActionSheet.hideActionSheet();
 				showToast(`Copied ${emojiNode.alt}'s URL to clipboard`, CopyMessageLink);
-			 }
+			}
 		},
 		...Platform.select({
 			ios: [
 				{
 					text: "Copy image to clipboard",
-					callback: () => { }
+					callback: () => fetchImageAsDataURL(emojiNode.src, (dataUrl) => {
+						setImage(dataUrl.split(',')[1]);
+						LazyActionSheet.hideActionSheet();
+						showToast(`Copied ${emojiNode.alt}'s image to clipboard`, CopyFile);
+					})
 				}
 			],
 			default: []
