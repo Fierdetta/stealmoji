52a495e3365ae56d95d5ad10fcf3b7e350b10224
slyde99 <slydentn@gmail.com>
2023-01-29 16:55:45 +0800

    Patch reactions sheet

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,21 @@
-import { findByProps } from "@vendetta/metro";
-import { after } from "@vendetta/patcher";
+import { find, findByProps } from "@vendetta/metro";
+import { ReactNative as RN } from "@vendetta/metro/common";
+import { after, before } from "@vendetta/patcher";
 import { FormDivider } from "@vendetta/ui/components/Forms";
 import StealButtons from "./components/StealButtons";
 
 const MessageEmojiActionSheet = findByProps("GuildDetails");
+const ActionSheet = find((m) => m.default?.render?.name == "ActionSheet")?.default;
 
 let unpatch;
+let unpatchReactions;
 
 export default {
   onLoad: () => {
     unpatch = after("default", MessageEmojiActionSheet, ([{ emojiNode }], res) => {
       // Don't do anything if the emoji doesn't have a image url
       if (!emojiNode.src) return;
-
+      
       // Contains everything about the emoji
       const EmojiInfo = res?.props?.children?.props?.children?.props?.children;
       // Obviously we don't want to patch if this isn't a thing
@@ -24,6 +27,15 @@ export default {
         React.useEffect(() => {
           return unpatchEmojiInfo;
         }, []);
+        
+        // const emoteDetails = res.props?.children[0]?.props?.children;
+        // if (emoteDetails && emoteDetails[0].type.name === "Icon") {
+          //   emoteDetails[0] = (
+        //     <RN.TouchableOpacity onPress={() => console.log(emojiNode)}>
+        //       {emoteDetails[0]}
+        //     </RN.TouchableOpacity>
+        //   )
+        // }
 
         // Append to the Add to Favorites button if it exists
         const buttonView = res.props?.children[3]?.props?.children;
@@ -46,8 +58,50 @@ export default {
         </>);
       })
     })
+
+    // Some old versions uses different kind of ActionSheet
+    if (ActionSheet) {
+      unpatchReactions = before("render", ActionSheet, ([props]) => {
+        // Checks if the action sheet is for message reactions
+        if (!props?.header?.props?.reactions || props.children.type?.name !== "FastList") return;
+        
+        // Patch the header
+        const unpatchReactionsHeader = after("type", props.header, (args, res) => {
+          // Unpatch on unmount
+          React.useEffect(() => unpatchReactionsHeader, []);
+    
+          const tabsRow = res.props.children[0];
+          const { tabs, onSelect } = tabsRow.props;
+          
+          // Wrap the tabs in a TouchableOpacity so we can add a long press handler
+          tabsRow.props.tabs = tabs.map((tab) => (
+            <RN.TouchableOpacity
+              onPress={() => onSelect(tab.props.index)}
+              onLongPress={() => console.log("Long pressed")}
+            >
+              {tab}
+            </RN.TouchableOpacity>
+          ));
+        });
+      });
+    }
+    
+    // // Kept for future reference..?
+    // before("default", findByDisplayName("v", false), ([tab]) => {
+    //   const { tabs, onSelect } = tab;
+  
+    //   tab.tabs = tabs.map((tab) => (
+    //     <RN.TouchableOpacity
+    //       onPress={() => onSelect(tab.props.index)}
+    //       onLongPress={() => showAddToServerActionSheet({})}
+    //     >
+    //       {tab}
+    //     </RN.TouchableOpacity>
+    //   ));
+    // });
   },
   onUnload: () => {
     unpatch();
+    unpatchReactions?.();
   },
 };
