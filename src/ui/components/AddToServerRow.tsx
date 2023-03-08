fc76538622bb588b7ffe804bcfe1718547fc0558
Jack Matthews <jm5112356@gmail.com>
2023-03-08 12:49:09 -0500

    Use input alert if the user wants to rename the emoji

--- src/ui/components/AddToServerRow.tsx
+++ src/ui/components/AddToServerRow.tsx
@@ -1,4 +1,5 @@
 import { findByDisplayName, findByProps } from "@vendetta/metro";
+import { showInputAlert } from "@vendetta/ui/alerts";
 import { getAssetIDByName } from "@vendetta/ui/assets";
 import { Forms } from "@vendetta/ui/components";
 import { showToast } from "@vendetta/ui/toasts";
@@ -13,21 +14,31 @@ const LazyActionSheet = findByProps("openLazy", "hideActionSheet");
 
 export default function AddToServerRow({ guild, emojiNode }) {
     const addToServerCallback = () => {
-        // Fetch image
-        fetchImageAsDataURL(emojiNode.src, (dataUrl) => {
-            // Upload it to Discord
-            Emojis.uploadEmoji({
-                guildId: guild.id,
-                image: dataUrl,
-                name: emojiNode.alt,
-                roles: undefined
-            }).then(() => {
-                // Let user know it was added
-                showToast(`Added ${emojiNode.alt} to ${guild.name}`, getAssetIDByName("Check"));
-                // Close the sheet
-                LazyActionSheet.hideActionSheet();
-            });
-        });
+        showInputAlert({
+            title: "Emoji name",
+            initialValue: emojiNode.alt,
+            placeholder: "bleh",
+            onConfirm: (name) => {
+                // Fetch image
+                fetchImageAsDataURL(emojiNode.src, (dataUrl) => {
+                    // Upload it to Discord
+                    Emojis.uploadEmoji({
+                        guildId: guild.id,
+                        image: dataUrl,
+                        name: name,
+                        roles: undefined
+                    }).then(() => {
+                        // Let user know it was added
+                        showToast(`Added ${emojiNode.alt} to ${guild.name}`, getAssetIDByName("Check"));
+                    });
+                });
+            },
+            confirmText: `Add to ${guild.name}`,
+            confirmColor: undefined,
+            cancelText: "Cancel"
+        })
+        // Close the sheet
+        LazyActionSheet.hideActionSheet();
     };
 
     return (<FormRow
